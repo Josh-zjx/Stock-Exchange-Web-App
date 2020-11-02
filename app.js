4ed6985b917d4af3313442f7c0516082f01b04ae
joshzhu <josh.zhujiaxu@gmail.com>
2020-11-01 19:27:50 -0800

    implement backend

--- app.js
+++ app.js
@@ -1,18 +1,77 @@
 var express = require("express");
-var http = require("http");
+var https = require("https");
+var cors = require("cors");
 var fs = require('fs');
 var url = require('url');
-
+const news_token = '7f20794f88a54122a84190892f8641e4'
+const stock_token = 'b121787d28d3e2a8ce8f38860d189dd20b9477b4'
 var app = express();
 app.use('/public',express.static('public'));
-
+app.use(cors());
 const port = 8080;
 
 
 app.get('/query',(req,res)=>{
-    var sticker = req.query.name;
-    console.log(`Received query on ${sticker}`);
-    res.send(`Results on ${sticker}`);
+    var type = req.query.type;
+    var ticker = req.query.name;
+    console.log(`Received query on ${ticker}`);
+    var options = {host:"api.tiingo.com"}
+    if(type=="daily"){
+        options.path = `/tiingo/daily/${ticker}?token=${stock_token}`;
+        
+    }
+    else if(type=="iex"){
+        options.path = `/iex/${ticker}?token=${stock_token}`;
+    }
+    else if(type=="dac"){
+        var date = new Date();
+        date.setDate(date.getDate() - 2);
+        var start_date = "";
+        start_date +=(date.getFullYear()).toString();
+        start_date +="-";
+        start_date +=(date.getMonth()+1).toString();
+        start_date +="-";
+        start_date +=date.getDate().toString();
+        options.path = `/iex/${ticker}/prices?startDate=${start_date}&resampleFreq=5min&token=${stock_token}`;
+    }
+    else if(type=="hist"){
+        var date = new Date();
+        var start_date = "";
+        start_date +=(date.getFullYear()-2).toString();
+        start_date +="-";
+        start_date +=(date.getMonth()+1).toString();
+        start_date +="-";
+        start_date +=date.getDate().toString();
+
+        options.path = `/tiingo/daily/${ticker}/prices?startDate=${start_date}&resampleFreq=daily&token=${stock_token}`;
+    }
+    else if(type=="news"){
+
+    }
+    else if(type=="ac")
+    {
+
+    }
+    else{
+        res.send("Unrecognized Token")
+        return 0;
+    }
+
+    https.get(options,(response)=>{
+        var body = '';
+        response.on('data', function(data) {
+            body += data;
+        });
+        response.on('end',()=>{
+            console.log(body);
+            res.send(body);
+        })
+        
+    })
+    
+    
+
+
 })
 app.get('*',(req,res)=>{
     fs.readFile("search.html",(err,data)=>{
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var server = require("./server");
-var router = require("./router");
- 
-server.start(router.route);
+var date = new Date();
+console.log(date.getDate())
+console.log(date.getMonth()-2)
+console.log(date.getFullYear())
+var start_date = "";
+        start_date +=(date.getFullYear()-2).toString();
+        start_date +="-";
+        start_date +=(date.getMonth()+1).toString();
+        start_date +="-";
+        start_date +=date.getDate().toString();
+        console.log(start_date)
